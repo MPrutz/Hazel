f3884db0560a1977b68c90a726d9f340d22c1ba9
mprutz@yahoo.com <MPrutz@yahoo.com>
2022-06-12 20:25:38 -0700

    Setup basic Application and Entry Points, From Yan Chernikov -The Cherno- on YouTube

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+//------- ENTRY  POINT -----------
+#include "Hazel/EntryPoint.h"
+//--------------------------------
